829bd30a08c5befa718ca745e950dae222fd17db
Amadeusz Blanik <amadeusz@blanik.me>
2021-12-22 00:17:33 +0100

    Added firstElement utility

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 export { default as clamp } from './source/clamp';
 export { default as compareObjects } from './source/compareObjects';
 export { default as debounce } from './source/debounce';
+export { default as firstElement } from './source/firstElement';
 export { default as isEmpty } from './source/isEmpty';
 export { default as randomBoolean } from './source/randomBoolean';
